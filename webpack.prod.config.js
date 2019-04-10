f6e640b61e20380f3f63b4faeebf6bc6dffab5ee
Gabriel Santos <mfkrgabriel@gmail.com>
2019-04-10 06:25:27 -0300

    Adding css loader and prod env

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,23 +5,26 @@ const webpack = require('webpack')
 const validate = require('webpack-validator')
 
 module.exports = validate({
-  devtool: 'source-map',
-
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, 'src', 'index')
-  ],
+  entry: path.join(__dirname, 'src', 'index'),
 
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    }),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+
   ],
 
   module: {
